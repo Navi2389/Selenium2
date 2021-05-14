5d9f73c69152ff9bef6edf791c31fe22c6dc22ea
Navi2389 <koluivan@yandex.ru>
2021-05-14 20:18:36 +0300

    Проект4.Исправленный2 Напишите функцию, вычисляющую площадь треугольника по трём сторонам (int a, int b, int c). Разместите класс с функцией в src/main/java. Разместите тесты на эту функцию в классе src/test/java/.../TriangleTest.java. Настройте генерацию отчёта и по желанию — логирование.

--- src/test/java/project4TriangleTest/FunctionsTests.java
+++ src/test/java/project4TriangleTest/FunctionsTests.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import project4TriangleFunction.NegativeOrNullTriangleSideException;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static project4TriangleFunction.Functions.calculateArea;
 
 public class FunctionsTests {
     @Test
     void positiveTriangleAreaCalculationTest() throws NegativeOrNullTriangleSideException {
-        Assertions.assertEquals(calculateArea(3,3,3),4);
+        Assertions.assertEquals(calculateArea(3,3,3),2.23606797749979);
         //сравниваем значение в скобках со значением после запятой
+    }
+
+    @Test
+    void degenerateTriangleTest(){
+        assertThatExceptionOfType(NegativeOrNullTriangleSideException.class).isThrownBy(()->calculateArea(-1,4,6));
     }
 }
